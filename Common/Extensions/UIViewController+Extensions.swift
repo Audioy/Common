f0ca89c735989e9c840dd90e674f0fbb2baa947c
John <audioltd@gmail.com>
2018-05-18 11:26:45 +0100

    Added a uiviewcontroller extension that takes care of adding/removing child view controllers

---
+++ Common/Extensions/UIViewController+Extensions.swift
@@ -0,0 +1,26 @@
+//
+//  UIViewController+Extensions.swift
+//  Common
+//
+//  Created by John Neumann on 18/05/2018.
+//  Copyright © 2018 Audioy. All rights reserved.
+//
+
+import Foundation
+
+extension UIViewController {
+    // Source: https://medium.com/@johnsundell/using-child-view-controllers-as-plugins-in-swift-458e6b277b54
+    func add(_ child: UIViewController) {
+        addChildViewController(child)
+        view.addSubview(child.view)
+        child.didMove(toParentViewController: self)
+    }
+    func remove() {
+        guard parent != nil else {
+            return
+        }
+        willMove(toParentViewController: nil)
+        removeFromParentViewController()
+        view.removeFromSuperview()
+    }
+}
